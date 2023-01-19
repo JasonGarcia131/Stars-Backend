2dde36d112e3ba441f3e0354b61876c73caf4fd5
Jason Garcia <jaygarcia0530@gmail.com>
2023-01-18 22:28:39 -0800

    updated mongo uri

--- config/dbConn.js
+++ config/dbConn.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
     try {
         await mongoose.set('strictQuery', false)
-        await mongoose.connect(`${process.env.DATABASE_URI}`, {
+        await mongoose.connect(process.env.DATABASE_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
