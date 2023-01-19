e6dfbed361116874fa0950c56f440323732959df
Jason Garcia <jaygarcia0530@gmail.com>
2023-01-18 21:15:33 -0800

    postcontroller casing issue

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const PostsController = require('../../controllers/PostsController');
+const postsController = require('../../controllers/postsController');
 const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles');
 const paginate = require("../../middleware/paginate");
@@ -8,17 +8,17 @@ const verifyJWT = require("../../middleware/verifyJWT");
 const Post = require("../../model/Post");
 
 router.route('/')
-    .get(PostsController.getAllPosts)
-    .post([verifyJWT, verifyRoles(ROLES_LIST.User)], PostsController.createPost)
+    .get(postsController.getAllPosts)
+    .post([verifyJWT, verifyRoles(ROLES_LIST.User)], postsController.createPost)
 
 router.route('/:id')
-    .delete([verifyJWT, verifyRoles(ROLES_LIST.User)], PostsController.deletePost);
+    .delete([verifyJWT, verifyRoles(ROLES_LIST.User)], postsController.deletePost);
 
 router.route('/paginate')
-    .get(paginate(Post), PostsController.getUserPosts);
+    .get(paginate(Post), postsController.getUserPosts);
 
 router.route('/paginate/public')
-    .get(paginate(Post), PostsController.getUserPosts);
+    .get(paginate(Post), postsController.getUserPosts);
 
 
 module.exports = router;
